296580f5f50d7b4a113168072f947d2c9e4fb855
Sreehari T Rajesh <sreehari_b200754cs@nitc.ac.in>
2025-04-27 13:25:15 +0530

    added files

--- internals/pkg/constants.go
+++ internals/pkg/constants.go
@@ -1,22 +1,22 @@
 package pkg
 
 const (
-	BRIDGE                        = "bridge"
-	VXLAN                         = "vxlan"
-	CONFIG_FILE_PATH              = "config.yaml"
-	BASE_IMAGE                    = "ubuntu:latest"
-	ORDER_0                       = 0
-	ORDER_1                       = 1
-	ORDER_2                       = 2
-	ORDER_3                       = 3
-	CHECKPOINT                    = "cp"
-	DEFAULT_CONTAINER_PORT        = "80/tcp"
-	MACVLAN_NETWORK_NAME          = "vxlan-network"
-	DEFAULT_DOCKER_CHECKPOINT_DIR = "/var/lib/docker/containers/%s/checkpoints"
-	DEFAULT_CHECKPOINT_DIR_PARENT = "/home/rajesh/RuntimeManager/checkpoint/"
+	BRIDGE                         = "bridge"
+	VXLAN                          = "vxlan"
+	CONFIG_FILE_PATH               = "config.yaml"
+	BASE_IMAGE                     = "ubuntu:latest"
+	ORDER_0                        = 0
+	ORDER_1                        = 1
+	ORDER_2                        = 2
+	ORDER_3                        = 3
+	CHECKPOINT                     = "cp"
+	DEFAULT_CONTAINER_PORT         = "80/tcp"
+	MACVLAN_NETWORK_NAME           = "vxlan-network"
+	DEFAULT_DOCKER_CHECKPOINT_DIR  = "/var/lib/docker/containers/%s/checkpoints"
+	DEFAULT_CHECKPOINT_DIR_PARENT  = "checkpoint/"
 	DEFAULT_DOCKER_CHECKPOINT_PATH = "/var/lib/docker/containers/%s/checkpoints/%s"
-	DEFAULT_CHECKPOINT_DIR        = "/home/rajesh/RuntimeManager/checkpoint/%s"
-	FILE_EXISTS                   = "file exists"
+	DEFAULT_CHECKPOINT_DIR         = "checkpoint/%s"
+	FILE_EXISTS                    = "file exists"
 )
 
 /*
