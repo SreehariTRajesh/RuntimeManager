641c4a54b2ee2c94f38061c1dffd544c66a23d6b
Sreehari T Rajesh <sreehari_b200754cs@nitc.ac.in>
2025-04-27 14:10:40 +0530

    added files

--- internals/pkg/constants.go
+++ internals/pkg/constants.go
@@ -13,9 +13,9 @@ const (
 	DEFAULT_CONTAINER_PORT         = "80/tcp"
 	MACVLAN_NETWORK_NAME           = "vxlan-network"
 	DEFAULT_DOCKER_CHECKPOINT_DIR  = "/var/lib/docker/containers/%s/checkpoints"
-	DEFAULT_CHECKPOINT_DIR_PARENT  = "checkpoint/"
+	DEFAULT_CHECKPOINT_DIR_PARENT  = "/home/rajesh/RuntimeManager/checkpoint/"
 	DEFAULT_DOCKER_CHECKPOINT_PATH = "/var/lib/docker/containers/%s/checkpoints/%s"
-	DEFAULT_CHECKPOINT_DIR         = "checkpoint/%s"
+	DEFAULT_CHECKPOINT_DIR         = "/home/rajesh/RuntimeManager/checkpoint/%s"
 	FILE_EXISTS                    = "file exists"
 )
 
